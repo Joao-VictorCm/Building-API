4322859d03a38faa294c0b8e517cb0bab1b490a3
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-11-07 22:07:17 -0300

    Add a rota que especifica a piada pelo id

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ app.get("/random", (req, res) =>{
 
 //2. GET a specific joke
 
+app.get("/jokes/id:", (req, res) =>{
+  const id = parseInt(req.params.id)
+  const foundJoke = jokes.find((jooke) => joke.id === id)
+  res.json(foundJoke)
+})
 //3. GET a jokes by filtering on the joke type
 
 //4. POST a new joke
