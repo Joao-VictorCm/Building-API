2d3a18821eacc5a19957be4a8fa700e717620806
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-11-11 21:42:10 -0300

    finalizado task delete uma piada com id especifico

--- index.js
+++ index.js
@@ -70,6 +70,19 @@ app.put("/jokes/:id", (req, res) =>{
 
 
 //7. DELETE Specific joke
+app.delete("/jokes/:id", (req, res) =>{
+  const id = parseInt(req.params.id)
+  const searchIndex = jokes.findIndex((joke) => joke.id === id)
+  if(searchIndex > -1){
+    jokes.splice(searchIndex, 1)
+    res.sendStatus(200)
+  }else{
+    res
+    .status(404)
+    .jspm({error: `Joke with id: ${id} not found`})
+  }
+})
+
 
 //8. DELETE All jokes
 
